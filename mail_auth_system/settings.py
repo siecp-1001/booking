24df37bb4f27248be38674d76eddd1b15fd8155c
Ahmed Ayman <108583424+siecp-1001@users.noreply.github.com>
2024-05-27 01:36:20 +0300

    up

--- mail_auth_system/settings.py
+++ mail_auth_system/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+     "corsheaders",
 
     'rest_framework',
     'djoser',
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -92,7 +94,10 @@ DATABASES = {
     }
 }
 
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+   
+]
 
 # email ----------------------------------
 
